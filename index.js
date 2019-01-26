0b1f7f345dcb318c51a08c53bc3b14f08c60aa42
Luke Edwards <luke.edwards05@gmail.com>
2019-01-26 15:01:00 -0800

    add module logic ~!

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-module.exports = function () {
-	//
-}
+const { createServer } = require('http');
+const { parse } = require('querystring');
+const send = require('@polka/send-type');
+const url = require('@polka/url');
+
+module.exports = function (handler) {
+	const $ = {
+		server: null,
+
+		async handler(req, res) {
+			const info = url(req);
+			req.path = info.pathname;
+			req.query = info.query ? parse(info.query) : {};
+			req.search = info.search;
+
+			const out = await handler(req, res);
+			res.finished || out && send(res, res.statusCode || 200, out);
+		},
+
+		listen() {
+			const handler = (r1, r2) => setImmediate($.handler.bind(null, r1, r2));
+			($.server = $.server || createServer()).on('request', handler);
+			return $.server.listen.apply($.server, arguments);
+		}
+	};
+
+	return $;
+};
