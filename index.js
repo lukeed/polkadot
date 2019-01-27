f6aafb7079904ed4379a26a42329cad3d5889109
Luke Edwards <luke.edwards05@gmail.com>
2019-01-27 11:10:08 -0800

    refactor: use `loop()` for Node 6.x support

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const { parse } = require('querystring');
 const send = require('@polka/send-type');
 const url = require('@polka/url');
 
+function loop(res, out) {
+	if (res.finished) return;
+	return out && out.then !== void 0
+		? out.then(d => loop(res, d)) : send(res, res.statusCode || 200, out);
+}
+
 module.exports = function (handler) {
 	const $ = {
 		server: null,
 
-		async handler(req, res) {
+		handler(req, res) {
 			const info = url(req);
 			req.path = info.pathname;
 			req.query = info.query ? parse(info.query) : {};
 			req.search = info.search;
 
-			const out = await handler(req, res);
-			res.finished || out && send(res, res.statusCode || 200, out);
+			loop(res, handler(req, res));
 		},
 
 		listen() {
@@ -25,4 +30,4 @@ module.exports = function (handler) {
 	};
 
 	return $;
-};
+}
