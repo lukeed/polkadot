c8d0cc95e121b719eaa51e02ce1cb0299f87e45f
Luke Edwards <luke.edwards05@gmail.com>
2019-01-26 18:22:53 -0800

    refactor(bin): use `require()` for implied resolution

--- bin.js
+++ bin.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 const { resolve } = require('path');
-const { existsSync } = require('fs');
-
-const { PORT=3000 } = process.env;
-const argv = process.argv.slice(2);
-const entry = resolve(argv.shift() || 'index.js');
-
-if (!existsSync(entry)) {
-	console.error('Entry file does not exist:', entry);
-	process.exit(1);
-}
-
-const handler = require(entry);
 const polkadot = require('.');
 
-polkadot(handler).listen(PORT, err => {
-	if (err) throw err;
-	console.log(`Running on localhost:${PORT}`);
-});
+const argv = process.argv.slice(2);
+const entry = resolve(argv.shift() || 'index.js');
+const { PORT=3000 } = process.env;
+
+try {
+	const handler = require(entry);
+	polkadot(handler).listen(PORT, err => {
+		if (err) throw err;
+		console.log(`Running on http://localhost:${PORT}`);
+	});
+} catch (err) {
+	if (err.code !== 'MODULE_NOT_FOUND') throw err;
+	console.error(err.message);
+	return process.exit(1);
+}
